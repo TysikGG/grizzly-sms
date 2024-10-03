b896a531feda8fd6a870a4101abfa61fc2a5e087
TysikGG <100859234+TysikGG@users.noreply.github.com>
2024-10-03 14:17:19 +0300

    New numbers DB, test.js update

--- test.js
+++ test.js
@@ -1,13 +1,24 @@
 const { GrizzlyInit } = require("./index.js");
 
-const apiKey = "6f56715a92eda88fa96ca6a377a26a76";
+const apiKey = "";
 
 const grizzly = new GrizzlyInit(apiKey);
 // saveFilePath: "./prices.json",
+
 const balance = grizzly.getLowestPrices({
+    saveFilePath: "./prices.json",
     countryCode: "6",
     serviceCode: "ds",
-    parseDelay: 100
+    parseDelay: 200,
+    minCount: 100,
+    maxPrice: 3
 });
 
 console.log(balance)
+
+
+const number = grizzly.getNumber({
+    countryCode: require("./prices.json")[0].code,
+    serviceCode: "ds"
+})
+
