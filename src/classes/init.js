a67cd7725d280a7d89c356d1a2cd53a5d7f2cf51
TysikGG <100859234+TysikGG@users.noreply.github.com>
2024-10-03 14:17:42 +0300

    GetLowestPrices new method, getNumber method

--- src/classes/init.js
+++ src/classes/init.js
@@ -5,6 +5,7 @@ const { APIRequest } = require("../functions/request");
 class GrizzlyInit {
     constructor (apiKey) {
         this.apiKey = apiKey;
+        this.numbers = {}
     }
 
     async getBalance() {
@@ -24,14 +25,11 @@ class GrizzlyInit {
         if (!data[countryCode]) return null;
         if (data.error) return data;
 
-        console.log(data)
         const formattedData = data[countryCode][serviceCode];
-        console.log(formattedData)
-
         return formattedData;
     };
 
-    async getLowestPrices({serviceCode, saveFilePath, parseDelay, retryErrors}) {
+    async getLowestPrices({serviceCode, saveFilePath, parseDelay, retryErrors, minCount, maxPrice}) {
         const infoList = [];
         let sortedInfoList = [];
         const countryCodes = getCodesList();
@@ -43,9 +41,12 @@ class GrizzlyInit {
             });
             if ((!data || !data?.cost) && retryErrors) return getInfo(code);
             else if (data && data?.cost) {
-                infoList.push(data)
+                if (data.count < (minCount || 0)) return;
+                if (data.cost > maxPrice) return;
+                data.code = code;
+                infoList.push(data);
                 return data;
-            }
+            };
         };
 
         async function delayedIterations(codes, delay, obj) {
@@ -61,6 +62,36 @@ class GrizzlyInit {
         console.log(sortedInfoList)
         if (saveFilePath) writeFileSync(saveFilePath, JSON.stringify(sortedInfoList, null, 2))
     };
+
+    async getNumber({serviceCode, countryCode, maxPrice}) {
+        if (maxPrice) {
+            const info = await this.getInfo({
+                serviceCode: serviceCode,
+                countryCode: countryCode
+            });
+            if (info?.cost > maxPrice) return;
+        };
+
+        const data = await APIRequest("getNumber", {
+            api_key: this.apiKey,
+            service: serviceCode,
+            country: countryCode
+        });
+
+        const splittedData = data.split(":");
+
+        this.numbers[splittedData[2]] = {
+            id: splittedData[1],
+            activatedTime: Date.now()
+        };
+        console.log(this.numbers);
+        return {
+            number: splittedData[2],
+            id: splittedData[1]
+        };
+    };
+
+    async 
 };
 
 module.exports = {
